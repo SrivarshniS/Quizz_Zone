b3ed661a40f103295497593ee417991eaf612067
Srivarshni S <mail.srivarshni@gmail.com>
2026-01-30 10:35:47 +0530

    Logo changed

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -30,7 +30,7 @@ export default function HomeScreen({ onStart, leaderboards }) {
 
       <div className="hero-section animate-float" style={{ textAlign: 'center', marginBottom: '4rem', zIndex: 2 }}>
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <img src={wtmLogo} alt="Women Techmakers Logo" style={{ maxWidth: '200px', marginBottom: '0.0rem', paddingRight: '1rem'}} />
+          <img src={wtmLogo} alt="Women Techmakers Logo" style={{ maxWidth: '200px', marginBottom: '0.0rem', paddingRight: '3rem'}} />
           <h1>X</h1>
           <img src={shebuilds} alt="Shebuilds Logo" style={{ maxWidth: '300px', marginBottom: '0.0rem'}} />
         </div>
